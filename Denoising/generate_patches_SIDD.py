18ed8f82afaa7aef44aa61883a8c5de5bed8b047
An Vuong <vuonga2@eng-kec3110-02.eecs.oregonstate.edu>
2024-10-31 12:43:20 -0700

    update for celeba

--- Denoising/generate_patches_SIDD.py
+++ Denoising/generate_patches_SIDD.py
@@ -11,20 +11,26 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate patches from Full Resolution images')
 parser.add_argument('--src_dir', default='../SIDD_Medium_Srgb/Data', type=str, help='Directory for full resolution images')
 parser.add_argument('--tar_dir', default='../SIDD_patches/train',type=str, help='Directory for image patches')
-parser.add_argument('--ps', default=256, type=int, help='Image Patch Size')
-parser.add_argument('--num_patches', default=300, type=int, help='Number of patches per image')
-parser.add_argument('--num_cores', default=10, type=int, help='Number of CPU Cores')
+# parser.add_argument('--ps', default=64, type=int, help='Image Patch Size')
+parser.add_argument('--num_patches', default=10, type=int, help='Number of patches per image')
+parser.add_argument('--num_cores', default=15, type=int, help='Number of CPU Cores')
+parser.add_argument('--timesteps', type=int, required=True)
+parser.add_argument('--type', type=str, required=True)
 
 args = parser.parse_args()
 
 src = args.src_dir
 tar = args.tar_dir
-PS = args.ps
+# PS = args.ps
+if args.type == "train":
+    PS = 64
+else:
+    PS = 128
 NUM_PATCHES = args.num_patches
 NUM_CORES = args.num_cores
 
 noisy_patchDir = os.path.join(tar, 'input')
-clean_patchDir = os.path.join(tar, 'groundtruth')
+clean_patchDir = os.path.join(tar, 'target')
 
 if os.path.exists(tar):
     os.system("rm -r {}".format(tar))
@@ -33,15 +39,18 @@ os.makedirs(noisy_patchDir)
 os.makedirs(clean_patchDir)
 
 #get sorted folders
-files = natsorted(glob(os.path.join(src, '*', '*.PNG')))
+files = natsorted(glob(os.path.join(src,'*', '*.png')))
 
 noisy_files, clean_files = [], []
 for file_ in files:
-    filename = os.path.split(file_)[-1]
-    if 'GT' in filename:
+    filename = os.path.split(file_)[-2]
+    if 'clean' in filename and args.type in filename:
         clean_files.append(file_)
-    if 'NOISY' in filename:
+    if 'noisy' in filename and args.type in filename and str(args.timesteps) in filename:
         noisy_files.append(file_)
+# print(clean_files[:100])
+# print(noisy_files[:100])
+print(f"Start processing {len(noisy_files)} files")
 
 def save_files(i):
     noisy_file, clean_file = noisy_files[i], clean_files[i]
